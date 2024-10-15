6869eacb4223e0f12a1e48e0a5ca9c59cb734799
sorza <alexandre.sorza@outlook.com>
2024-10-14 21:08:58 -0300

    Padronizando Responses

---
+++ AgencyManager.Core/Configuration.cs
@@ -0,0 +1,13 @@
+namespace AgencyManager.Core
+{
+    public static class Configuration
+    {
+        public const int DefaultStatusCode = 200;
+        public const int DefaultPageSize = 25;
+        public const int DefaultPageNumber = 1;
+
+        public static string ConnectionString { get;  set; } = string.Empty;
+        public static string FrontendUrl { get; set; } = string.Empty;
+        public static string BackendUrl { get; set; } = string.Empty;
+    }
+}
