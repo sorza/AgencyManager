3901e35bb5ef4620c959d6320efd260a41e58399
sorza <alexandre.sorza@outlook.com>
2024-10-26 22:49:21 -0300

    Criando UpdateCashRequest e implementando seus testes de entrada

--- AgencyManager.Core/Requests/Cash/UpdateCashRequest.cs
+++ AgencyManager.Core/Requests/Cash/UpdateCashRequest.cs
@@ -1,7 +1,23 @@
+using Flunt.Validations;
+
 namespace AgencyManager.Core.Requests.Cash
 {
-    public class UpdateCashRequest
+    public class UpdateCashRequest : Request
     {
-        
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal StartValue { get; set; }
+        public decimal EndValue { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<UpdateCashRequest>().Requires()  
+                .AreNotEquals(Id, Guid.Empty, "Identificador de caixa inválido")   
+                .IsEmail(UserId, "UserId", "Não é um email válido.")          
+                .IsLowerOrEqualsThan(Date, DateTime.Now, "A data não pode ser futura")
+                .IsGreaterOrEqualsThan(StartValue, 0, "StartValue", "O troco inicial deve ser positivo")
+                .IsGreaterOrEqualsThan(EndValue, 0, "EndValue", "O troco final deve ser positivo")
+            );
+        }
     }
 }
