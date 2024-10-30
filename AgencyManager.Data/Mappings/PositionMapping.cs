a452a61081138c9cf60fadc42b9e5b8c2c2a1c36
sorza <alexandre.sorza@outlook.com>
2024-10-29 23:51:58 -0300

    PositionMapping

---
+++ AgencyManager.Data/Mappings/PositionMapping.cs
@@ -0,0 +1,38 @@
+﻿using AgencyManager.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AgencyManager.Data.Mappings
+{
+    internal class PositionMapping : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.ToTable("Locality");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Description)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(70);
+
+            builder.Property(x => x.Responsabilities)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(500);
+
+            builder.Property(x => x.Salary)
+                .IsRequired(true)
+                .HasColumnType("DECIMAL");
+
+            builder.Property(x => x.AgencyId)
+                .IsRequired(true)
+                .HasColumnType("INT");
+
+            builder.HasOne(p => p.Agency)
+                .WithMany()
+                .HasForeignKey(p => p.AgencyId);
+        }
+    }
+}
