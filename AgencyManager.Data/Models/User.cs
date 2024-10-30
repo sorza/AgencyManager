90654e60923f6715227fe09881c775ac45ea4ba8
sorza <alexandre.sorza@outlook.com>
2024-10-30 00:18:13 -0300

    Mapeando Identity

---
+++ AgencyManager.Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AgencyManager.Data.Models
+{
+    internal class User : IdentityUser<long>
+    {
+        public List<IdentityRole<long>>? Roles { get; set; }
+    }
+}
