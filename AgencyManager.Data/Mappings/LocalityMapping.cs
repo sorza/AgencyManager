456af22d1416b3b3fae4e7965a90039cc182af1e
sorza <alexandre.sorza@outlook.com>
2024-10-29 23:48:07 -0300

    Locality Mapping

---
+++ AgencyManager.Data/Mappings/LocalityMapping.cs
@@ -0,0 +1,26 @@
+﻿using AgencyManager.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AgencyManager.Data.Mappings
+{
+    internal class LocalityMapping : IEntityTypeConfiguration<Locality>
+    {
+        public void Configure(EntityTypeBuilder<Locality> builder)
+        {
+            builder.ToTable("Locality");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.City)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.State)
+                .IsRequired(true)
+                .HasColumnType("CHAR")
+                .HasMaxLength(2);
+        }
+    }
+}
