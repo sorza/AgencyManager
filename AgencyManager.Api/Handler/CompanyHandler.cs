227b25ee5fe534c6968e542e2ca50426f11bdb43
sorza <alexandre.sorza@outlook.com>
2024-11-22 22:13:37 -0300

    CompanyHandler/UpdateAsync

--- AgencyManager.Api/Handler/CompanyHandler.cs
+++ AgencyManager.Api/Handler/CompanyHandler.cs
@@ -87,9 +87,56 @@ namespace AgencyManager.Api.Handler
             throw new NotImplementedException();
         }
 
-        public async Task<Response<Company>> UpdateAsync(UpdateCompanyRequest request)
+        public async Task<Response<Company?>> UpdateAsync(UpdateCompanyRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                #region 01. Buscar empresa
+                var company = await context.Companies
+                   .Include(a => a.Address)
+                   .Include(a => a.Contacts)
+                   .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                if (company is null)
+                    return new Response<Company?>(null, 404, "Empresa não encontrada");
+
+                #endregion
+
+                #region 02. Validar requisição
+                var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
+                var validationResults = new List<ValidationResult>();
+                string errors = string.Empty;
+
+                if (!Validator.TryValidateObject(request, validationContext, validationResults, true))
+                    return new Response<Company?>(null, 400, string.Join(". ", validationResults.Select(r => r.ErrorMessage)));
+
+                #endregion
+
+                #region 03. Remover Contatos
+                if(company.Contacts is not null)
+                    context.Contacts.RemoveRange(company.Contacts);
+
+                #endregion
+
+                #region 04. Mapear dados e atualizar dados
+                mapper.Map(request, company);
+
+                #endregion
+
+                #region 05. Salvar alterações
+                await context.SaveChangesAsync();
+
+                #endregion
+
+                #region 06. Retornar Resposta
+                return new Response<Company?>(company, 200, "Empresa atualizada com sucesso");
+
+                #endregion
+            }
+            catch
+            {
+                return new Response<Company?>(null, 500, "Não foi possível alterar a empresa");
+            }
         }
     }
 }
