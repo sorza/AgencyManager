56ccc36f5fb507f5129f2759eeaa196ab982b65d
sorza <alexandre.sorza@outlook.com>
2024-11-22 23:00:06 -0300

    CashHandler/CreateAsync

--- AgencyManager.Api/Handler/CashHandler.cs
+++ AgencyManager.Api/Handler/CashHandler.cs
@@ -1,15 +1,43 @@
-﻿using AgencyManager.Core.Handlers;
+﻿using AgencyManager.Api.Data;
+using AgencyManager.Core.Handlers;
 using AgencyManager.Core.Models.Entities;
 using AgencyManager.Core.Requests.Cash;
 using AgencyManager.Core.Responses;
+using AutoMapper;
+using System.ComponentModel.DataAnnotations;
 
 namespace AgencyManager.Api.Handler
 {
-    public class CashHandler : ICashHandler
+    public class CashHandler(AppDbContext context, IMapper mapper) : ICashHandler
     {
-        public Task<Response<Cash?>> CreateAsync(CreateCashRequest request)
+        public async Task<Response<Cash?>> CreateAsync(CreateCashRequest request)
         {
-            throw new NotImplementedException();
+            #region 01. Validar requisição
+
+            var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+            string errors = string.Empty;
+
+            if (!Validator.TryValidateObject(request, validationContext, validationResults, true))
+                return new Response<Cash?>(null, 400, string.Join(". ", validationResults.Select(r => r.ErrorMessage)));
+
+            #endregion
+
+            #region 02. Mapear propriedades e criar caixa
+            try
+            {
+                var cash = mapper.Map<Cash>(request);
+
+                await context.Cash.AddAsync(cash);
+                await context.SaveChangesAsync();
+
+                return new Response<Cash?>(cash, 201, "Caixa aberto com sucesso.");
+            }
+            catch
+            {
+                return new Response<Cash?>(null, 500, "Não foi possível abrir caixa");
+            }
+            #endregion
         }
 
         public Task<Response<Cash?>> DeleteAsync(DeleteCashRequest request)
---
+++ AgencyManager.Core/Profiles/CashProfile.cs
@@ -0,0 +1,15 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.Cash;
+using AutoMapper;
+
+namespace AgencyManager.Core.Profiles
+{
+    public class CashProfile : Profile
+    {
+        public CashProfile()
+        {
+            CreateMap<CreateCashRequest, Cash>();
+            CreateMap<UpdateCashRequest, Cash>();
+        }
+    }
+}
