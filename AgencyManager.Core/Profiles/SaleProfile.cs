dda3d140cac57f44580b87ec58bf10ff8321dbbb
sorza <alexandre.sorza@outlook.com>
2024-11-25 22:18:55 -0300

    Criando SaleProfile

---
+++ AgencyManager.Core/Profiles/SaleProfile.cs
@@ -0,0 +1,15 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.Sale;
+using AutoMapper;
+
+namespace AgencyManager.Core.Profiles
+{
+    public class SaleProfile : Profile
+    {
+        public SaleProfile()
+        {
+            CreateMap<CreateSaleRequest, Sale>();
+            CreateMap<UpdateSaleRequest, Sale>();
+        }
+    }
+}
