79619d24503820ca65d31cf3c1bdc379e53a6b0b
sorza <alexandre.sorza@outlook.com>
2024-11-25 22:56:10 -0300

    Criando Transaction Profile

---
+++ AgencyManager.Core/Profiles/TransactionProfile.cs
@@ -0,0 +1,15 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.Transaction;
+using AutoMapper;
+
+namespace AgencyManager.Core.Profiles
+{
+    public class TransactionProfile : Profile
+    {
+        public TransactionProfile()
+        {
+            CreateMap<CreateTransactionRequest, Transaction>();
+            CreateMap<UpdateTransactionRequest, Transaction>();
+        }
+    }
+}
