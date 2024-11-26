0cb137a07b4b0a7d5a15eb6cf527c0198dd9f639
sorza <alexandre.sorza@outlook.com>
2024-11-25 22:54:01 -0300

    Implementando metodos de SaleHandler

--- AgencyManager.Api/Handler/TransactionHandler.cs
+++ AgencyManager.Api/Handler/TransactionHandler.cs
@@ -1,35 +1,184 @@
-﻿using AgencyManager.Core.Handlers;
+﻿using AgencyManager.Api.Data;
+using AgencyManager.Core.Handlers;
+using AgencyManager.Core.Models.Entities;
 using AgencyManager.Core.Requests.Transaction;
 using AgencyManager.Core.Responses;
-using System.Transactions;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace AgencyManager.Api.Handler
 {
-    public class TransactionHandler : ITransactionHandler
+    public class TransactionHandler(AppDbContext context, IMapper mapper) : ITransactionHandler
     {
-        public Task<Response<Transaction>> CreateAsync(CreateTransactionRequest request)
+        public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                #region 01. Validar transação
+                var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
+                var validationResults = new List<ValidationResult>();
+                string errors = string.Empty;
 
-        public Task<Response<Transaction>> DeleteAsync(DeleteTransactionRequest request)
-        {
-            throw new NotImplementedException();
-        }
+                if (!Validator.TryValidateObject(request, validationContext, validationResults, true))
+                    return new Response<Transaction?>(null, 400, string.Join(". ", validationResults.Select(r => r.ErrorMessage)));
+                #endregion
 
-        public Task<PagedResponse<List<Transaction>?>> GetAllByCashAsync(GetTransactionsByCashId request)
-        {
-            throw new NotImplementedException();
-        }
+                #region 02. Mapear dados para transação
+                var transaction = mapper.Map<Transaction>(request);
 
-        public Task<Response<Transaction>> GetByIdAsync(GetTransactionByIdRequest request)
-        {
-            throw new NotImplementedException();
-        }
+                #endregion
 
-        public Task<Response<Transaction>> UpdateAsync(UpdateTransactionRequest request)
+                #region 03. Adicionar transação
+                await context.Transactions.AddAsync(transaction);
+                await context.SaveChangesAsync();
+
+                #endregion
+
+                #region 04. Retornar resposta
+                return new Response<Transaction?>(transaction, 200, "Transação registrada com sucesso!");
+
+                #endregion
+            }
+            catch
+            {
+                return new Response<Transaction?>(null, 500, "Não foi possível registrar a transação");
+            }
+        }
+        public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                #region 01. Buscar transação
+                var transaction = await context.Transactions.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                #endregion
+
+                #region 02. Verificar se é nulo
+                if (transaction is null) return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+                #endregion
+
+                #region 03. Excluir transação
+                context.Transactions.Remove(transaction);
+                await context.SaveChangesAsync();
+
+                #endregion
+
+                #region 04. Retornar resposta
+                return new Response<Transaction?>(transaction, 200, "Transação excluída com sucesso");
+
+                #endregion
+            }
+            catch
+            {
+                return new Response<Transaction?>(null, 500, "Não foi possível excluir a transação");
+            }
+        }
+        public async Task<PagedResponse<List<Transaction>?>> GetAllByCashAsync(GetTransactionsByCashId request)
+        {
+            try
+            {
+                #region 01. Buscar transações por caixa
+                var query = context
+                .Transactions
+                .Where(x => x.CashId == request.CashId)
+                .AsNoTracking();
+
+                #endregion
+
+                #region 02. Paginar de acordo com o especificado
+                var transactions = await query
+                    .Skip(request.PageSize * (request.PageNumber - 1))
+                    .Take(request.PageSize)
+                    .ToListAsync();
+
+                #endregion
+
+                #region 03. Obter quantidade de transações
+                var count = await query.CountAsync();
+
+                #endregion
+
+                #region 04. Retornar Resposta
+                return transactions is null
+                        ? new PagedResponse<List<Transaction>?>(null, 404, "Não foram encontradas transações para este caixa.")
+                        : new PagedResponse<List<Transaction>?>(transactions, count, request.PageNumber, request.PageSize);
+                #endregion
+            }
+            catch
+            {
+                return new PagedResponse<List<Transaction>?>(null, 500, "Não possível consultar as transações.");
+            }
+        }
+        public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+        {
+            try
+            {
+                #region 01. Buscar transação
+                var transaction = await context
+                    .Transactions
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                if (transaction is null)
+                    return new Response<Transaction?>(null, 404, "Transação não encontrada");
+                #endregion
+
+                #region 02. Retorna resposta
+                return new Response<Transaction?>(transaction, 200);
+
+                #endregion
+            }
+            catch
+            {
+                return new Response<Transaction?>(null, 500, "Não foi possível buscar a transação");
+            }
+        }
+        public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+        {
+            try
+            {
+                #region 01. Buscar Transação
+                var transaction = await context
+                    .Transactions
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                if (transaction is null)
+                    return new Response<Transaction?>(null, 404, "Transação não encontrada");
+                #endregion
+
+                #region 02. Validar Transação
+
+                var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
+                var validationResults = new List<ValidationResult>();
+                string errors = string.Empty;
+
+                if (!Validator.TryValidateObject(request, validationContext, validationResults, true))
+                {
+                    return new Response<Transaction?>(null, 400, string.Join(". ", validationResults.Select(r => r.ErrorMessage)));
+                }
+
+                #endregion
+
+                #region 03. Transferir os dados da Requisição para a transação                
+                mapper.Map(request, transaction);
+
+                #endregion
+
+                #region 04. Salvar alterações
+                await context.SaveChangesAsync();
+
+                #endregion
+
+                #region 05. Retornar Resposta
+                return new Response<Transaction?>(transaction, 200, "Transação atualizada com sucesso");
+
+                #endregion
+            }
+            catch
+            {
+                return new Response<Transaction?>(null, 500, "Não foi possível atualizar a transação");
+            }
         }
     }
 }
--- AgencyManager.Core/Handlers/ITransactionHandler.cs
+++ AgencyManager.Core/Handlers/ITransactionHandler.cs
@@ -1,4 +1,4 @@
-using System.Transactions;
+using AgencyManager.Core.Models.Entities;
 using AgencyManager.Core.Requests.Transaction;
 using AgencyManager.Core.Responses;
 
@@ -6,10 +6,10 @@ namespace AgencyManager.Core.Handlers
 {
     public interface ITransactionHandler
     {
-        Task<Response<Transaction>> CreateAsync (CreateTransactionRequest request);
-        Task<Response<Transaction>> UpdateAsync (UpdateTransactionRequest request);
-        Task<Response<Transaction>> DeleteAsync (DeleteTransactionRequest request);
-        Task<Response<Transaction>> GetByIdAsync(GetTransactionByIdRequest request);
+        Task<Response<Transaction?>> CreateAsync (CreateTransactionRequest request);
+        Task<Response<Transaction?>> UpdateAsync (UpdateTransactionRequest request);
+        Task<Response<Transaction?>> DeleteAsync (DeleteTransactionRequest request);
+        Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request);
         Task<PagedResponse<List<Transaction>?>> GetAllByCashAsync (GetTransactionsByCashId request);
     }
 }
