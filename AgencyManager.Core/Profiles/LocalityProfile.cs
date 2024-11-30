808fc57c8f274fee6b945e4c3469ed53c68163c7
sorza <alexandre.sorza@outlook.com>
2024-11-29 22:48:58 -0300

    Criando LocalityProfile

---
+++ AgencyManager.Core/Profiles/LocalityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.Locality;
+using AutoMapper;
+namespace AgencyManager.Core.Profiles
+{
+    public class LocalityProfile : Profile
+    {
+        public LocalityProfile()
+        {
+            CreateMap<CreateLocalityRequest, Locality>();
+            CreateMap<UpdateLocalityRequest, Locality>();
+        }
+    }
+}
