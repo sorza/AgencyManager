b8b472f71d5f1a310cb4c65e48d123606a031af9
sorza <alexandre.sorza@outlook.com>
2024-11-29 22:50:47 -0300

    Criando LocalityHandler

---
+++ AgencyManager.Api/Handler/LocalityHandler.cs
@@ -0,0 +1,35 @@
+﻿using AgencyManager.Core.Handlers;
+using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.Locality;
+using AgencyManager.Core.Responses;
+
+namespace AgencyManager.Api.Handler
+{
+    public class LocalityHandler : ILocalityHandler
+    {
+        public Task<Response<Locality>> CreateAsync(CreateLocalityRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Locality>> DeleteAsync(DeleteLocalityRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Locality>?>> GetAllAsync(GetAllLocalitiesRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Locality>> GetByIdAsync(GetLocalityByIdRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Locality>> UpdateAsync(UpdateLocalityRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
