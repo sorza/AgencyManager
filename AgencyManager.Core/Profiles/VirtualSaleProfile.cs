e42db9bdd54c1398f2bc9684ffff71088d989dcb
sorza <alexandre.sorza@outlook.com>
2024-11-29 22:35:44 -0300

    Implementando VirtualSaleProfile

---
+++ AgencyManager.Core/Profiles/VirtualSaleProfile.cs
@@ -0,0 +1,15 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.VirtualSale;
+using AutoMapper;
+
+namespace AgencyManager.Core.Profiles
+{
+    public class VirtualSaleProfile : Profile
+    {
+        public VirtualSaleProfile()
+        {
+            CreateMap<CreateVirtualSaleRequest, VirtualSale>();
+            CreateMap<UpdateVirtualSaleRequest, VirtualSale>();
+        }
+    }
+}
