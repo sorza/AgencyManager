0012f68ce3b108ccf93d2170469c80cb12f03a8f
sorza <alexandre.sorza@outlook.com>
2024-12-01 22:19:18 -0300

    Criando Profile para ContractService

---
+++ AgencyManager.Core/Profiles/ContractProfile.cs
@@ -0,0 +1,15 @@
+﻿using AgencyManager.Core.Models.Entities;
+using AgencyManager.Core.Requests.ContractService;
+using AutoMapper;
+
+namespace AgencyManager.Core.Profiles
+{
+    public class ContractProfile : Profile
+    {
+        public ContractProfile()
+        {
+            CreateMap<CreateContractServiceRequest, ContractService>();
+            CreateMap<UpdateContractServiceRequest, ContractService>();
+        }
+    }
+}
---
+++ AgencyManager.Core/Requests/ContractService/GetContractByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace AgencyManager.Core.Requests.ContractService
+{
+    public class GetContractByIdRequest : Request
+    {
+        public int Id { get; set; }
+    }
+}
