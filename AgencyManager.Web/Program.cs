0754814620a4b1d1485565ef1aed599b8ead6ecb
sorza <alexandre.sorza@outlook.com>
2024-12-28 23:01:37 -0300

    Criando CookieHandler e Registrando servico

--- AgencyManager.Web/Program.cs
+++ AgencyManager.Web/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AgencyManager.Web;
 using MudBlazor.Services;
+using AgencyManager.Web.Security;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<CookieHandler>();
 builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
