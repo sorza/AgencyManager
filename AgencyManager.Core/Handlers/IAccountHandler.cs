49564158fe20ea037b461e8a161bcfa8b394144d
sorza <alexandre.sorza@outlook.com>
2024-12-28 21:49:49 -0300

    Definindo servicos de login, logout e registro para o frontend

--- AgencyManager.Core/Handlers/IAccountHandler.cs
+++ AgencyManager.Core/Handlers/IAccountHandler.cs
@@ -6,6 +6,7 @@ namespace AgencyManager.Core.Handlers
     public interface IAccountHandler
     {
         Task<Response<string>> LoginAsync(LoginRequest request);
+        Task<Response<string>> RegisterAsync(RegisterRequest request);
         Task LogoutAsync();
     }
 }
---
+++ AgencyManager.Core/Requests/Account/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgencyManager.Core.Requests.Account
+{
+    public class RegisterRequest : Request
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Senha inválida.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
