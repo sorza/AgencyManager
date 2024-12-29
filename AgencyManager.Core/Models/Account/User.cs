25532351c510fec3b3d1ff4f84cb999e68d28310
sorza <alexandre.sorza@outlook.com>
2024-12-28 22:53:51 -0300

    Implementando CookieAuthenticationStateProvider

--- AgencyManager.Core/Models/Account/User.cs
+++ AgencyManager.Core/Models/Account/User.cs
@@ -3,6 +3,7 @@
     public class User
     {
         public string Email { get; set; } = string.Empty;
+        public bool IsEmailConfirmed { get; set; }
         public Dictionary<string, string> Claims { get; set; } = [];
     }
 }
