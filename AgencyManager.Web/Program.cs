2f44f39e9713f9a71e19ae4433dae4bf6e6bc34f
sorza <alexandre.sorza@outlook.com>
2024-12-29 16:49:20 -0300

    Implementando RegisterPage

--- AgencyManager.Web/Program.cs
+++ AgencyManager.Web/Program.cs
@@ -4,6 +4,7 @@ using AgencyManager.Web;
 using MudBlazor.Services;
 using AgencyManager.Web.Security;
 using Microsoft.AspNetCore.Components.Authorization;
+using AgencyManager.Core.Handlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -15,8 +16,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<CookieHandler>();
 
 builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
 
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
 builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)
                     x.GetRequiredService<CookieAuthenticationStateProvider>());
 
@@ -26,5 +27,7 @@ builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
 {
     opt.BaseAddress = new Uri(Configuration.BackendUrl);
 }).AddHttpMessageHandler<CookieHandler>();
+
+builder.Services.AddTransient<IAccountHandler, IAccountHandler>();
 
 await builder.Build().RunAsync();
